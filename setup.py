77a0ccf427b286c551fd4e41540b0e89581ff8a1
Satoru SATOH <satoru.satoh@gmail.com>
2020-03-12 12:27:43 +0900

    enhancmenet: add packaging stuff

---
+++ setup.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+import os.path
+import os
+import setuptools
+import setuptools.command.bdist_rpm
+
+
+NAME = "ansiblelint_custom_rules"
+VERSION = "0.1.0"
+
+# For daily snapshot versioning mode:
+if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
+    import datetime
+    VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
+
+
+class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
+    """Override the default content of the RPM SPEC.
+    """
+    spec_tmpl = os.path.join(os.path.abspath(os.curdir), "package.spec.in")
+
+    def _replace(self, line):
+        """Replace some strings in the RPM SPEC template"""
+        if "@VERSION@" in line:
+            return line.replace("@VERSION@", VERSION)
+
+        if "Source0:" in line:  # Dirty hack
+            return "Source0: %{pkgname}-%{version}.tar.gz"
+
+        return line
+
+    def _make_spec_file(self):
+        return [self._replace(l.rstrip()) for l
+                in open(self.spec_tmpl).readlines()]
+
+
+setuptools.setup(name=NAME, version=VERSION,
+                 cmdclass=dict(bdist_rpm=bdist_rpm),
+                 packages=[NAME], package_dir={NAME: "rules"})
+
+# vim:sw=4:ts=4:et:
