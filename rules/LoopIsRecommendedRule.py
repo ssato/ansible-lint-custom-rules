1c22cdcf47ac9388120c090780704498e3b482e1
Satoru SATOH <satoru.satoh@gmail.com>
2020-03-24 14:58:52 +0900

    enhancement: add an experiemental rule impl. to test if with_* is used in tasks, and its test cases

---
+++ rules/LoopIsRecommendedRule.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2020 Red Hat, Inc.
+#
+# SPDX-License-Identifier: MIT
+#
+"""Lint rule class to test if tasks use with_* directives.
+"""
+import ansiblelint
+
+
+_RULE_ID = "Custom_2020_5"
+_DESC = """loop is recommended and use of with_* may be repalced with it.
+See also:
+https://docs.ansible.com/ansible/latest/user_guide/playbooks_loops.html
+"""
+
+
+def with_directive_is_used(_self, _file, task):
+    """
+    .. seealso:: ansiblelint.AnsibleLintRule.matchtasks
+    """
+    with_st = [key for key in task if key.startswith("with_")]
+    if with_st:
+        return "Use of {} was found".format(with_st)
+
+    return False
+
+
+class LoopIsRecommendedRule(ansiblelint.AnsibleLintRule):
+    """
+    Rule class to test if any tasks use with_* loop directive.
+    """
+    id = _RULE_ID
+    shortdesc = "loop is recommended and with_* may be repalced with it"
+    description = _DESC
+    severity = "LOW"
+    tags = ["readability", "formatting"]
+    version_added = "4.2.99"  # dummy
+
+    matchtask = with_directive_is_used
