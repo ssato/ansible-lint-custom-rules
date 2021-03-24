6674e52e76a766862cb86cb9ad3ae5ceb3c74950
Satoru SATOH <satoru.satoh@gmail.com>
2021-03-25 05:20:11 +0900

    change: cleanup listing functions, rule and cli test case base classes

--- tests/common.py
+++ tests/common.py
@@ -4,9 +4,11 @@
 # pylint: disable=invalid-name,missing-function-docstring
 """Common utility test routines and classes.
 """
+import functools
 import os
 import pathlib
 import subprocess
+import typing
 import unittest
 
 import ansiblelint.utils
@@ -25,54 +27,83 @@ DEFAULT_RULES_DIR: str = str(
 )
 
 
-def list_res_files(path_pattern):
+def strip_words(astr: str, *words: str) -> str:
+    """strip given words from ``astr`` one by one.
     """
-    List resource data files.
+    return functools.reduce(lambda acc, word: acc.replace(word, ''),
+                            words, astr)
+
+
+def list_resources(name: str, success: bool = True,
+                   search: typing.Optional[str] = None
+                   ) -> typing.List[str]:
     """
-    files = sorted(str(p) for p in (CURDIR / "res").glob(path_pattern))
+    List resource data files for OK or NG test cases.
+    """
+    if not search:
+        search = 'ok' if success else 'ng'
+
+    pattern = f"*{search}*.*"
+    files = sorted(str(p) for p in (CURDIR / 'res' / name).glob(pattern))
     if not files:
-        raise RuntimeError("No data files: " + path_pattern)
+        raise RuntimeError(f"No resource data files: { pattern }")
 
     return files
 
 
-class AnsibleLintRuleTestCase(unittest.TestCase):
+def get_rule_name(file_: str = __file__) -> str:
+    """
+    Resolve the name of the rule to test from the filename of the test code.
+    """
+    return strip_words(pathlib.Path(file_).name, '.py', 'test_', 'Test')
+
+
+def get_rule_instance_by_name(rule_module, rule_name):
+    rule_cls = getattr(rule_module, rule_name)
+    if not rule_cls:
+        raise ValueError(f"No such rule class {rule_name} "
+                         f"in {rule_module!r}.")
+    return rule_cls()
+
+
+class RuleTestCase(unittest.TestCase):
     """Base class to test ansible-lint rules.
     """
+    name = None
     rule = None
-    prefix = None
-    clear_fn = None
+    clear_fn: typing.Optional[typing.Callable] = None
 
     def setUp(self):
         """Initialize lint rules collection.
         """
-        # Default rules only
-        self.rules = RulesCollection()
-        self.rules.register(self.rule)  # Register the rule explicitly.
-
-        if callable(self.clear_fn):
-            self.clear_fn()
-
-    def path_pattern(self, rtype="ok"):
-        """
-        Make up a file (glob) path pattern.
-        """
-        return "{}/*{}*.yml".format(self.prefix, rtype)
-
-    def lint(self, expected_success=True, ppattern=None):
-        """
-        :param playbook_fn_patterns: Glob filenames pattern to find playbooks
-        """
-        if not self.prefix or not self.rule:
+        if not self.rule or not self.name:
             return
 
-        if ppattern is None or not ppattern:
-            ppattern = self.path_pattern("ok" if expected_success else "ng")
+        # Collect the default rules and add the rule to test.
+        self.rules = RulesCollection()
+        self.rules.register(self.rule)
 
-        for filepath in list_res_files(ppattern):
+        if callable(self.clear_fn):
+            self.clear_fn()  # pylint: disable=not-callable
+
+    def list_resources(self, success: bool = True,
+                       search: typing.Optional[str] = None
+                       ) -> typing.List[str]:
+        return list_resources(self.name, success=success,
+                              search=search)
+
+    def lint(self, success: bool = True,
+             search: typing.Optional[str] = None):
+        """
+        Run the lint rule's check to given resource data files.
+        """
+        if not self.rule or not self.name:
+            return
+
+        for filepath in self.list_resources(success, search=search):
             runner = Runner(self.rules, filepath, [], [], [])
             res = runner.run()
-            if expected_success:
+            if success:
                 self.assertEqual(0, len(res), res)  # No errors
             else:
                 self.assertTrue(len(res) > 0, res)  # Something went wrong
@@ -84,7 +115,7 @@ class AnsibleLintRuleTestCase(unittest.TestCase):
         self.lint(False)
 
 
-def _rule_ids_itr():
+def list_rule_ids_itr() -> typing.Iterator[str]:
     """
     Yield custom rule IDs using ansiblelint.rules.RulesCollection.
     """
@@ -98,36 +129,37 @@ def _rule_ids_itr():
             yield rule.id
 
 
-class AnsibleLintRuleCliTestCase(AnsibleLintRuleTestCase):
+class CliTestCase(RuleTestCase):
     """Run ok and ng CLI test cases automatically.
     """
     def setUp(self):
-        super(AnsibleLintRuleCliTestCase, self).setUp()
+        super().setUp()
 
         excl_opt = ' '.join(("-x {!s}".format(rid)
-                             for rid in _rule_ids_itr()
+                             for rid in list_rule_ids_itr()
                              if rid != getattr(self.rule, "id", None)))
-        self.cmd = "ansible-lint -r {} {}".format(RULES_DIR, excl_opt).split()
+        self.cmd = f"ansible-lint -r { RULES_DIR } { excl_opt }".split()
 
-    def lint(self, expected_success=True, ppattern=None, env=None):
+    # pylint: disable=arguments-differ
+    def lint(self, success: bool = True,
+             search: typing.Optional[str] = None,
+             env: typing.Optional[typing.Dict] = None):
         """
         Run ansible-lint with given arguments in given env.
         """
-        if self.rule is None or not self.prefix:
+        if not self.rule or not self.name:
             return
 
-        if ppattern is None or not ppattern:
-            ppattern = self.path_pattern("ok" if expected_success else "ng")
-
+        oenv = os.environ.copy()
         if env:
-            os.environ.update(**env)
+            oenv.update(**env)
 
-        for filepath in list_res_files(ppattern):
+        for filepath in self.list_resources(success, search=search):
             res = subprocess.run(
                 self.cmd + [filepath], stdout=subprocess.PIPE, check=False,
-                env=os.environ
+                env=oenv
             )
-            if expected_success:
+            if success:
                 self.assertEqual(res.returncode, 0, res.stdout.decode("utf-8"))
             else:
                 self.assertNotEqual(res.returncode, 0,
