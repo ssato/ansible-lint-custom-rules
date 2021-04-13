b3dfb74203b4b9085ce137f026e63c9f1a799a08
Satoru SATOH <satoru.satoh@gmail.com>
2021-04-13 17:00:03 +0900

    change/fix: rename from BlacklistedModuleRule to BlockedModules and update its test cases along with some additions of test resource data

---
+++ rules/BlockedModules.py
@@ -0,0 +1,105 @@
+# Copyright (C) 2020,2021 Satoru SATOH <satoru.satoh@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+#
+r"""
+Lint rule class to test if some blocked modules were used.
+
+Users can change the behavior of this rule by specifying a environment
+variable, _ANSIBLE_LINT_RULE_BLOCKED_MODULES.
+
+- An example to list blocked modules with the env. variable.
+
+  ::
+
+      _ANSIBLE_LINT_RULE_BLOCKED_MODULES="shell raw"
+
+- An example to list blocked modules with the file given in the env. variable.
+
+  ::
+
+      _ANSIBLE_LINT_RULE_BLOCKED_MODULES="@/tmp/mb.list"
+
+  The file must contain module names to be blockeded line by line, and comments
+  in the file starts with '#' are ignored like this:
+
+  ::
+
+     # this is a comment line.
+     shell
+     include
+
+.. seealso:: :class:`~ansiblielint.rules.DeprecatedModuleRule`
+"""
+import functools
+import os
+import re
+import typing
+import warnings
+
+import ansiblelint.rules
+
+
+ID: str = 'blockeded-modules'
+ENV_VAR = '_ANSIBLE_LINT_RULE_' + ID.upper().replace('-', '_')
+
+BLOCKED_MODULES: typing.FrozenSet[str] = frozenset("""
+shell
+include
+""".split())
+
+MODULES_RE = re.compile(r'^(\w+(?:\s+\w+)*)$', re.ASCII)
+
+
+@functools.lru_cache(maxsize=32)
+def blocked_modules(default: typing.FrozenSet[str] = BLOCKED_MODULES
+                    ) -> typing.FrozenSet[str]:
+    """
+    Get and return the blocked modules from the env. var, file or default.
+    """
+    blocked = os.environ.get(ENV_VAR, False)
+    if blocked:
+        if blocked.startswith('@'):  # It's a file path.
+            path = blocked[1:]
+            try:
+                res = frozenset(line.strip() for line in open(path)
+                                if line.strip() and not line.startswith('#'))
+                if res:
+                    return res
+            except (IOError, OSError) as exc:
+                warnings.warn('Failed to load blocked modules from '
+                              f'{path}, exc={exc!s}')
+        else:
+            match = MODULES_RE.match(blocked)
+            if match:
+                return frozenset(match.groups()[0].split())
+
+    return default
+
+
+class BlockedModules(ansiblelint.rules.AnsibleLintRule):
+    """
+    Lint rule class to test if variables defined by users follow the namging
+    conventions and guildelines.
+    """
+    id: str = ID
+    shortdesc: str = 'Blocked modules'
+    description: str = 'Use of the blockeded modules are prohivited.'
+    severity: str = 'HIGH'
+    tags: typing.List[str] = [ID, 'module']
+
+    def matchtask(self, task: typing.Dict[str, typing.Any]
+                  ) -> typing.Union[bool, str]:
+        """
+        .. seealso:: ansiblelint.rules.AnsibleLintRule.matchtasks
+        """
+        try:
+            mod = task['action']['__ansible_module__']
+            if mod in blocked_modules():
+                return f'{self.shortdesc}: {mod}'
+        except KeyError:
+            pass
+
+        return False
+
+# vim:sw=4:ts=4:et:
---
+++ tests/TestBlockedModules.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2020,2021 Red Hat, Inc.
+# SPDX-License-Identifier: MIT
+#
+# pylint: disable=invalid-name
+# pylint: disable=too-few-public-methods,missing-class-docstring
+# pylint: disable=missing-function-docstring
+"""Test cases for the rule, BlockedModules.
+"""
+import pytest
+
+from rules import BlockedModules as TT
+from tests import common
+
+
+class Base:
+    this_py: common.MaybeModNameT = __file__
+    this_mod: common.MaybeModT = TT
+    clear_fn: common.MaybeCallableT = TT.blocked_modules.cache_clear
+
+
+@pytest.mark.parametrize(
+    'evalue,expected',
+    [('', TT.BLOCKED_MODULES),
+     ('ping', frozenset(('ping', ))),
+     ('ping script', frozenset(('ping', 'script'))),
+     ('@/dev/null', TT.BLOCKED_MODULES),
+     ]
+)
+def test_blockeded_modules(evalue, expected, monkeypatch):
+    monkeypatch.setenv(TT.ENV_VAR, evalue)
+    assert TT.blocked_modules() == expected
+    Base.clear_fn()
+
+
+@pytest.mark.parametrize(
+    'content,expected',
+    [('#\n\n', TT.BLOCKED_MODULES),
+     ('ping\n', frozenset(('ping', ))),
+     ('#\nping\nscript\n', frozenset(('ping', 'script'))),
+     ]
+)
+def test_blockeded_modules_from_file(content, expected, tmp_path, monkeypatch):
+    path = tmp_path / 'blocked_modules.list'
+    path.write_text(content)
+
+    monkeypatch.setenv(TT.ENV_VAR, f'@{path!s}')
+    assert TT.blocked_modules() == expected
+    Base.clear_fn()
+
+
+# 'ping' is listed in it.
+LIST_FILE = common.TESTS_DIR / 'res' / 'BlockedModules' / 'blocked_modules.txt'
+
+
+class RuleTestCase(Base, common.RuleTestCase):
+    def test_30_ng_cases__env(self):
+        self.lint(False, 'ok', env={TT.ENV_VAR: 'ping'})
+
+    def test_40_ng_cases__file(self):
+        self.lint(False, 'ok', env={TT.ENV_VAR: f'@{LIST_FILE!s}'})
+
+
+class CliTestCase(Base, common.CliTestCase):
+    def test_30_ng_cases__env(self):
+        self.lint(False, 'ok', env={TT.ENV_VAR: 'ping'})
+
+    def test_40_ng_cases__file(self):
+        self.lint(False, 'ok', env={TT.ENV_VAR: f'@{LIST_FILE!s}'})
