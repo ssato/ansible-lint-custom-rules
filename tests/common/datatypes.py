159061515139e149d592851b6033a02ad94a5d82
Satoru SATOH <satoru.satoh@gmail.com>
2021-07-11 00:53:19 +0900

    change: replace tests.common.datatypes.TData with .SubData
    Replace tests.common.datatypes.TData with .SubData because we will not
    load any lintables explicitely during tests, so we don't need a path
    information for input files like TData provides.

--- tests/common/datatypes.py
+++ tests/common/datatypes.py
@@ -1,20 +1,16 @@
 # Copyright (C) 2020, 2021 Satoru SATOH <satoru.satoh@gmail.com>
 # SPDX-License-Identifier: MIT
 #
-"""Common utility test routines and classes - utilities.
+# pylint: disable=inherit-non-class
+"""Common utility functios and classes - datatypes.
 """
-import pathlib
 import typing
 
 
-DataT = typing.Iterable[typing.Optional[pathlib.Path]]
-
-
-class TData(typing.NamedTuple):
-    """A namedtuple object keeps test data info."""
-    datadir: pathlib.Path
-    inpath: pathlib.Path
+class SubData(typing.NamedTuple):
+    """A namedtuple object keep environment variables and configs.
+    """
     conf: typing.Dict[str, typing.Any]
-    env: typing.Dict[str, typing.Any]
+    env: typing.Dict[str, str]
 
 # vim:sw=4:ts=4:et:
