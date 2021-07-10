7b0f55ecdaef0f1eb7a9abb837fcd05e322a82c8
Satoru SATOH <satoru.satoh@gmail.com>
2021-07-11 03:15:21 +0900

    change: replace file based runner with dir based runners in tests.common.runner
    Replace file based runner, RunFromFile, with dir based runners,
    RuleRunner and CliRunner, in tests.common.runner.
    
    Dir based runners will get test data dir, run in that dir and find
    lintables (playbooks, etc.) automatically under the dir.
    
    And added test cases for the runners.

--- tests/common/runner.py
+++ tests/common/runner.py
@@ -4,91 +4,156 @@
 # pylint: disable=invalid-name,too-few-public-methods
 """Common utility test routines and classes.
 """
+import os
 import pathlib
+import subprocess
+import tempfile
 import typing
+import unittest.mock
 
 import ansiblelint.config
 import ansiblelint.constants
 import ansiblelint.errors
-import ansiblelint.file_utils
+import ansiblelint.rules
 import ansiblelint.runner
+import yaml
 
-from ansiblelint.rules import AnsibleLintRule, RulesCollection
-
-from . import constants
-
-
-def each_rule_ids(*rdirs: str, use_default: bool = False
-                  ) -> typing.Iterator[str]:
-    """List the IDs of rules in given dirs.
-    """
-    if rdirs:
-        rdirs = list(rdirs)
-    else:
-        rdirs = [str(constants.RULES_DIR)]
-
-    if use_default:
-        rdirs.append(ansiblelint.constants.DEFAULT_RULESDIR)
-
-    for rule in ansiblelint.rules.RulesCollection(rdirs):
-        yield rule.id
+from . import utils
 
 
-RuleOptionsT = typing.Optional[typing.Dict[str, typing.Any]]
-
-
-def get_collection(rule: typing.Optional[AnsibleLintRule] = None,
-                   rule_options: RuleOptionsT = None,
-                   use_default_rules: bool = False
-                   ) -> RulesCollection:
-    """
-    Get RulesCollection instance with given rule registered.
-    """
-    if use_default_rules:
-        rulesdirs = [ansiblelint.constants.DEFAULT_RULESDIR]
-    else:
-        rulesdirs = []
-
-    options = ansiblelint.config.options
-    options.enable_list = ['no-same-owner']
-
-    try:
-        collection = RulesCollection(rulesdirs=rulesdirs, options=options)
-    except TypeError:
-        collection = RulesCollection(rulesdirs=rulesdirs)
-
-    if rule:
-        # Hack to force setting options.
-        setattr(
-            ansiblelint.config.options, 'rules', {rule.id: rule_options or {}}
-        )
-        collection.register(rule)
-
-    return collection
-
-
-class RunFromFile:
-    """Base Class to run ansiblelint for given files.
+class RuleRunner:
+    """Base Class to run ansiblelint without a lintable in a dir.
 
     .. seealso:: ansiblelint.testing.RunFromText
     """
-    def __init__(self, collection: RulesCollection):
+    def __init__(self, rule: ansiblelint.rules.AnsibleLintRule,
+                 rules_dir: pathlib.Path,
+                 skip_list: typing.Optional[typing.List[str]] = None,
+                 enable_default: bool = False):
         """Initialize an instance with given rules collection.
-        """
-        self._collection = collection
 
-    def run_playbook(self, path: typing.Union[str, pathlib.Path],
-                     skip_list: typing.Optional[typing.List[str]] = None
-                     ) -> typing.List[ansiblelint.errors.MatchError]:
-        """Lints received playbook file.
+        :param rule: An AnsibleLintRule instance to test
+        :param rules_dir: The path to a dir contains custom rules
+        :param skip_list: A list of rule IDs to disable (skip)
+        :param enable_default:
+            True if default rules will be enabled also
         """
-        if skip_list is None:
-            skip_list = []
+        self.rule = rule
 
-        return ansiblelint.runner.Runner(
-            ansiblelint.file_utils.Lintable(path, kind='playbook'),
-            rules=self._collection,
-            skip_list=skip_list
-        ).run()
+        self.rulesdirs = [str(rules_dir.resolve())]
+        if enable_default:
+            self.rulesdirs.append(ansiblelint.constants.DEFAULT_RULESDIR)
+
+        self.skip_list = skip_list.copy()
+
+        options = ansiblelint.config.options
+        # .. seealso:: ansiblelint.testing.fixtures.default_rules_collection
+        options.enable_list = ['no-same-owner']
+        self.rules = ansiblelint.rules.RulesCollection(
+            rulesdirs=self.rulesdirs, options=options
+        )
+
+    def get_skip_list(self, isolated: bool = True) -> typing.List[str]:
+        """Get a list of rule IDs to skip.
+
+        :param isolated: True if to disable other rules
+        """
+        skip_list = self.skip_list.copy() if self.skip_list else []
+        if isolated:
+            skip_list.extend([
+                r.id for r in self.rules if r.id != self.rule.id
+            ])
+
+        return skip_list
+
+    def run(self, workdir: pathlib.Path, isolated: bool = True
+            ) -> typing.List[ansiblelint.errors.MatchError]:
+        """Lint in the workdir.
+
+        :param workdir: Working dir to run runner later
+        :param isolated: True if to disable other rules
+        """
+        with utils.chdir(workdir.resolve()):
+            lintables = ansiblelint.utils.get_lintables(
+                options=ansiblelint.config.options
+            )
+            assert bool(
+                lintables
+            ), f'No lintables were found at {workdir!s}'
+
+            sub_data = utils.load_sub_data_in_dir()
+
+            rid = self.rule.id
+            # Hack to force setting options for the rule.
+            setattr(
+                ansiblelint.config.options, 'rules',
+                {rid: sub_data.conf.get('rules', {}).get(rid, {})}
+            )
+
+            runner = ansiblelint.runner.Runner(
+                *lintables, rules=self.rules,
+                skip_list=self.get_skip_list(isolated)
+            )
+
+            if sub_data.env:
+                env = utils.get_env(sub_data.env)
+                with unittest.mock.patch.dict(os.environ, env, clear=True):
+                    return runner.run()
+
+            return runner.run()
+
+
+class CliRunner(RuleRunner):
+    """Base Class to run ansiblelint without a lintable in a dir.
+
+    .. seealso:: RuleRunner
+    """
+    def __init__(self, rule: ansiblelint.rules.AnsibleLintRule,
+                 rules_dir: pathlib.Path,
+                 skip_list: typing.Optional[typing.List[str]] = None,
+                 enable_default: bool = False):
+        """Initialize an instance with given rules collection.
+
+        :param rule: An AnsibleLintRule instance to test
+        :param rules_dir: The path to a dir contains custom rules
+        :param skip_list: A list of rule IDs to disable (skip)
+        :param enable_default:
+            True if default rules will be enabled also
+        """
+        super().__init__(rule, rules_dir, skip_list, enable_default)
+
+        self.cmd = ['ansible-lint', '-r', f'{rules_dir.resolve()!s}']
+        if enable_default:
+            self.cmd.append('-R')
+
+    def run(self, workdir: pathlib.Path, isolated: bool = True
+            ) -> typing.Tuple[int, str, str]:
+        """Run Ansible Lint in the workdir.
+
+        :param workdir: Working dir to run runner later
+        :param isolated: True if to disable other rules
+
+        .. seealso:: ansiblelint.testing.run_ansible_lint
+        """
+        workdir = workdir.resolve()
+        sub_data = utils.load_sub_data_in_dir(workdir)
+
+        conf = sub_data.conf if sub_data.conf else dict()
+        env = utils.get_env(sub_data.env or {})
+
+        skip_list = self.get_skip_list(isolated)
+        if skip_list:
+            conf['skip_list'] = skip_list
+
+        with tempfile.NamedTemporaryFile(mode='w') as cio:
+            yaml.safe_dump(conf, cio)
+
+            opts = dict(stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                        check=False, shell=False, env=env, cwd=str(workdir),
+                        universal_newlines=True)
+
+            # pylint: disable=subprocess-run-check
+            res = subprocess.run(self.cmd + ['-c', cio.name], **opts)
+            return (res.returncode, res.stdout, res.stderr)
 
 # vim:sw=4:ts=4:et:
--- tests/common/test_runner.py
+++ tests/common/test_runner.py
@@ -1,55 +1,121 @@
 # Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
 # SPDX-License-Identifier: MIT
 #
-# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name,missing-function-docstring
 """Test cases of tests.common.runner.
 """
-import ansiblelint.rules
 import pytest
 
+from ansiblelint.rules.DeprecatedModuleRule import DeprecatedModuleRule
+from rules.BlockedModules import ID as OTHER_CUSTOM_RULE_ID_EX
 from rules.DebugRule import DebugRule
-from tests.common import runner as TT
+from tests.common import constants, runner as TT
 
 
-# ansiblelint.rules.IncludeMissingFileRule:
-STD_RULE_EX_ID: str = 'deprecated-module'
+# ansiblelint.rules.DeprecatedModuleRule:
+DEFAULT_RULE_ID_EX: str = DeprecatedModuleRule.id
 
 
 @pytest.mark.parametrize(
-    ('rdirs', 'use_default', 'rule_id_ex'),
-    (([], False, DebugRule.id),
-     ([], True, STD_RULE_EX_ID),
-     ([str(TT.constants.RULES_DIR)], False, DebugRule.id),
-     ([str(TT.constants.RULES_DIR)], True, STD_RULE_EX_ID),
+    ('rules_dir', 'skip_list', 'enable_default'),
+    ((constants.RULES_DIR, None, False),
+     (constants.RULES_DIR, [OTHER_CUSTOM_RULE_ID_EX], False),
+     (constants.RULES_DIR, None, True),
      )
 )
-def test_each_rule_ids(rdirs, use_default, rule_id_ex):
-    res = list(TT.each_rule_ids(*rdirs, use_default=use_default))
-    assert res, res
-    assert rule_id_ex in res, f'{res!r}'
+def test_RuleRunner__init__(rules_dir, skip_list, enable_default):
+    runner = TT.RuleRunner(
+        DebugRule(), rules_dir, skip_list=skip_list,
+        enable_default=enable_default
+    )
+    assert runner.rule.id == DebugRule.id, runner.rule.id
+    assert runner.rules
+
+    rule_ids = [r.id for r in runner.rules]
+    assert DebugRule.id in rule_ids, rule_ids
+
+    if skip_list:
+        assert runner.skip_list == skip_list, runner.skip_list
+
+    if enable_default:
+        assert DEFAULT_RULE_ID_EX in rule_ids, rule_ids
 
 
 @pytest.mark.parametrize(
-    'rule,options',
-    [(DebugRule(), None),
-     (DebugRule(), dict(enabled=True)),
-     (None, None),
-     ]
+    ('rules_dir', 'skip_list', 'isolated'),
+    ((constants.RULES_DIR, None, True),
+     (constants.RULES_DIR, [OTHER_CUSTOM_RULE_ID_EX], True),
+     (constants.RULES_DIR, None, True),
+     (constants.RULES_DIR, None, False),
+     (constants.RULES_DIR, [OTHER_CUSTOM_RULE_ID_EX], False),
+     (constants.RULES_DIR, None, False),
+     )
 )
-def test_get_collection(rule, options):
-    collection = TT.get_collection(rule, rule_options=options)
-    assert isinstance(collection, ansiblelint.rules.RulesCollection)
+def test_RuleRunner_get_skip_list(rules_dir, skip_list, isolated):
+    runner = TT.RuleRunner(
+        DebugRule(), rules_dir, skip_list=skip_list, enable_default=True
+    )
+    skips = runner.get_skip_list(isolated=isolated)
 
-    rules = list(collection)
-    assert len(rules) > 0  # It has default rules even if `rule` is None.
+    assert DebugRule.id not in skips, skips
 
-    if rule:
-        rules = [r for r in collection if r.id == rule.id]
-        assert len(rules) == 1
+    if skip_list:
+        assert all(s in skips for s in skip_list), skips
 
-        if options:
-            # pylint: disable=no-member
-            grops = TT.ansiblelint.config.options.rules[rule.id]
-            assert sorted(grops.items()) == sorted(options.items())
+    if isolated:
+        assert DebugRule.id not in skips, skips
+        assert DEFAULT_RULE_ID_EX in skips, skips
+
+
+DEBUG_RES_DIR = constants.TESTS_RES_DIR / 'DebugRule'
+
+
+# see tests/res/DebugRule/{ok,ng}/...
+@pytest.mark.parametrize(
+    ('workdir', 'isolated', 'exp'),
+    ((DEBUG_RES_DIR / 'ok/0', True, True),
+     (DEBUG_RES_DIR / 'ok/0', False, True),
+     (DEBUG_RES_DIR / 'ng/0', True, False),
+     (DEBUG_RES_DIR / 'ng/0', False, False),
+     )
+)
+def test_RuleRunner_run(workdir, isolated, exp):
+    runner = TT.RuleRunner(DebugRule(), constants.RULES_DIR)
+    res = runner.run(workdir, isolated=isolated)
+    assert not res if exp else res, res
+
+
+@pytest.mark.parametrize(
+    ('enable_default', ),
+    ((False, ),
+     (True, ),
+     )
+)
+def test_CliRunner__init__(enable_default):
+    runner = TT.CliRunner(
+        DebugRule(), constants.RULES_DIR, enable_default=enable_default
+    )
+    assert str(constants.RULES_DIR.resolve()) in runner.cmd
+
+    if enable_default:
+        assert '-R' in runner.cmd
+
+
+@pytest.mark.parametrize(
+    ('workdir', 'exp'),
+    ((DEBUG_RES_DIR / 'ok/0', True),
+     (DEBUG_RES_DIR / 'ng/0', False),
+     )
+)
+def test_CliRunner_run(workdir, exp):
+    runner = TT.CliRunner(DebugRule(), constants.RULES_DIR)
+    res = runner.run(workdir)
+    if exp:
+        assert res[0] == 0
+        assert res[1:] == ('', '')
+    else:
+        assert res[0] != 0
+        assert bool(res[1])
+        assert bool(res[2])
 
 # vim:sw=4:ts=4:et:
