5d5e4efba6ee021e8975eb26b2fdfa91efa6f77d
Satoru SATOH <satoru.satoh@gmail.com>
2021-07-11 00:51:14 +0900

    change: add a global variable to set environemt vars can be set safely
    Add a global variable tests.constants.SAFE_ENV_VARS to set environemt
    vars can be set from os.environemt safely.

--- tests/common/constants.py
+++ tests/common/constants.py
@@ -4,11 +4,24 @@
 """Common utility test routines and classes - Some constants.
 """
 import pathlib
+import typing
 
 
 TESTS_DIR: pathlib.Path = pathlib.Path(__file__).parent.parent.resolve()
 TESTS_RES_DIR = TESTS_DIR / 'res'
 
 RULES_DIR = TESTS_DIR.parent / 'rules'
+
+# .. seealso:: ansiblelint.testsing.run_ansible_lint
+SAFE_ENV_VARS: typing.Iterable[str] = (
+    'LANG',
+    'LC_ALL',
+    'LC_CTYPE',
+    'NO_COLOR',
+    'PATH',
+    'PYTHONIOENCODING',
+    'PYTHONPATH',
+    'TERM',
+)
 
 # vim:sw=4:ts=4:et:
