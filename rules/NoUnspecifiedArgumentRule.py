95f555aa029cf4d78f8bb218b23a57429ceba0b5
Guido Grazioli <ggraziol@redhat.com>
2022-02-03 18:04:03 +0100

    Improve rule to return line numbers

--- rules/NoUnspecifiedArgumentRule.py
+++ rules/NoUnspecifiedArgumentRule.py
@@ -8,12 +8,19 @@ import functools
 import re
 import typing
 import warnings
+import yaml
 
 import ansiblelint.rules
 
-from pathlib import Path
-from ansiblelint.utils import parse_yaml_from_file, nested_items
+from yaml.composer import Composer
+from yaml.constructor import Constructor
+from yaml.nodes import ScalarNode
+from yaml.resolver import BaseResolver
+from yaml.loader import SafeLoader
 
+from pathlib import Path
+from ansiblelint.utils import parse_yaml_linenumbers, parse_yaml_from_file, nested_items
+from ansiblelint.utils import LINE_NUMBER_KEY
 
 if typing.TYPE_CHECKING:
     from ansiblelint.constants import odict
@@ -41,38 +48,49 @@ DESC: str = r"""Rule to test if all role arguments are specified in meta/argumen
 def _lookup_argument_specs(var_file: Path, var_name: str) -> bool:
     """
     Find arg specification and lookup variable name is present.
-    """	
+    """
+    meta_data: Dict[str, Any] = {}
+
     if var_file.is_file():
-        argument_specs: Path = var_file.parent / ".." / "meta" / "argument_specs.yml"
-    print('lookup var_name = %s' % var_name )
-    if argument_specs.is_file():
-        meta_data = parse_yaml_from_file(str(argument_specs))
-        print(meta_data)
-        if meta_data:
-            try:
-            	if meta_data["argument_specs"]["main"]["options"]:
-                    return var_name in meta_data["argument_specs"]["main"]["options"]
-            except KeyError:
-                return False
+        argument_specs_path: Path = var_file.parent / ".." / "meta" / "argument_specs.yml"
+        argument_specs = str(argument_specs_path)
+
+    if argument_specs_path.is_file() and not argument_specs in meta_data.keys():
+        meta_data[argument_specs] = parse_yaml_from_file(argument_specs)
+
+    if argument_specs in meta_data.keys():
+        try:
+            if meta_data[argument_specs]["argument_specs"]["main"]["options"]:
+                return var_name in meta_data[argument_specs]["argument_specs"]["main"]["options"]
+        except KeyError:
+            return False
+
     return False
 
 
-def each_keys(data: 'odict[str, typing.Any]') -> typing.Iterator[str]:
-    """
-    Traverse nested dict and yield keys. 
-    todo: nested_items is deprecated
-    """
-    for key, _val, _parent in nested_items(data):
-        # Special cases.
-        # .. seealso:: ansiblelint.utils.nested_items
-        if key in ['list-item', '__line__', '__file__']:
-            continue
+class LineLoader(SafeLoader):
+    def __init__(self, stream):
+        super(LineLoader, self).__init__(stream)
 
-        if isinstance(_val, dict) or _parent:
-            continue
+    def compose_node(self, parent, index):
+        # the line number where the previous token has ended (plus empty lines)
+        line = self.line
+        node = Composer.compose_node(self, parent, index)
+        node.__line__ = line + 1
+        return node
 
-        print("==== key: %s  ==== val: %s  ==== par: %s" % (key, _val, _parent))
-        yield key, _parent+'.' if _parent else ''
+    def construct_mapping(self, node, deep=False):
+        node_pair_lst = node.value
+        node_pair_lst_for_appending = []
+
+        for key_node, value_node in node_pair_lst:
+            shadow_key_node = ScalarNode(tag=BaseResolver.DEFAULT_SCALAR_TAG, value='__line__' + key_node.value)
+            shadow_value_node = ScalarNode(tag=BaseResolver.DEFAULT_SCALAR_TAG, value=key_node.__line__)
+            node_pair_lst_for_appending.append((shadow_key_node, shadow_value_node))
+
+        node.value = node_pair_lst + node_pair_lst_for_appending
+        mapping = Constructor.construct_mapping(self, node, deep=deep)
+        return mapping
 
 
 class NoUnspecifiedArgumentRule(ansiblelint.rules.AnsibleLintRule):
@@ -86,20 +104,20 @@ class NoUnspecifiedArgumentRule(ansiblelint.rules.AnsibleLintRule):
     severity = 'HIGH'
     tags = [ID,'metadata','readability']
 
-    def matchplay(self, file: 'Lintable',
-                  data: 'odict[str, typing.Any]'
-                  ) -> typing.List['MatchError']:
-        """
-        .. seealso:; ansiblelint.rules.AnsibleLintRule.matchplay
-        """
-        if file.kind == 'vars':
-            print('data: %s' % data)
-            return [
-                self.create_matcherror(
-                    details=f'{self.shortdesc}: {parent}{var_name}', filename=file
-                )
-                for var_name, parent in each_keys(data)
-                if not _lookup_argument_specs(file.path, var_name)
-            ]
+    def matchyaml(self, file: 'Lintable') -> typing.List['MatchError']:
+        """Return matches for variables defined in vars files with no specification."""
+        results: List["MatchError"] = []
 
-        return []
+        if file.kind == 'vars':
+            with open(str(file.path), 'r') as f:
+                variables = yaml.load(f, Loader=LineLoader)
+                for var_name in filter(lambda k: not k.startswith('__line__') and not isinstance(variables[k],dict), variables.keys()):
+                    if not _lookup_argument_specs(file.path, var_name):
+                        results.append(
+                            self.create_matcherror(
+                                details=f'{self.shortdesc}: {var_name}', filename=file, linenumber=variables["__line__"+var_name]
+                            )
+                        )
+        else:
+            results.extend(super().matchyaml(file))
+        return results
